f91cf540ae9771f5b82cf2f28c2698003f0829ef
aaron <315485099@qq.com>
2016-03-15 20:07:51 +0800

    test

--- app/component.jsx
+++ app/component.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default React.createClass({
     render: function () {
-        return <h1>Hello world! haohao</h1>
+        return <h1>Hello world! test</h1>
     }
 });
