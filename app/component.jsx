aeab8ccf2ce06c312cc8fb44408dc45787179d84
aaron <315485099@qq.com>
2016-03-17 09:43:40 +0800

    add router

--- app/component.jsx
+++ app/component.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default React.createClass({
     render: function () {
-        return <h1>Hello world! test</h1>
+        return <h1>Hello world! ES6</h1>
     }
 });
--- app/main.js
+++ app/main.js
@@ -1,5 +1,66 @@
 import React from 'react';
 import Hello from './component.jsx';
-var ReactDOM = require('react-dom');
+import { render } from 'react-dom';
+import { Router, Route, Link, IndexRoute } from 'react-router';
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var App = React.createClass({ 
+	render: function() {
+		return(
+			<div>
+				<header>
+			        <ul>
+			            <li><Link to="/app">Dashboard</Link></li>
+			            <li><Link to="/inbox">Inbox</Link></li>
+			            <li><Link to="/calendar">calendar</Link></li>
+			        </ul>
+			        Logged in as Jane
+		        </header>
+		        { this.props.children }
+			</div>
+		);
+	}
+});
+
+var Dashboard = React.createClass({
+	render: function () {
+	    return (
+		    <div>
+		        <p>Dashboard</p>
+		    </div>
+	    );
+	}
+});
+
+var Inbox = React.createClass({
+	  render: function () {
+	    return (
+		    <div>
+		        <p>Inbox</p>
+		    </div>
+	    );
+	}
+});
+
+var Calendar = React.createClass({
+	render: function () {
+	    return (
+		    <div>
+		        <p>Calendar</p>
+		    </div>
+	    );
+	}
+});
+
+render((
+	<Router>
+	    <Route path="/" component={App}>
+		    <IndexRoute component={Dashboard}/>
+		    <Route path="app" component={Dashboard}/>
+		    <Route path="inbox" component={Inbox}/>
+		    <Route path="calendar" component={Calendar}/>
+		    <Route path="*" component={Dashboard}/>
+		    </Route>
+	</Router>
+), document.body);
+
+// ReactDOM.render(<Hello />, document.getElementById('app'));
